b19372fa9172886e47405968487ce9c6d37a17dd
Kibarski Jonathan <phenixdoc@gmail.com>
2013-03-27 23:32:48 +0200

    gitstatus.py: changed sumbols to ascii

--- .bash/gitstatus.py
+++ .bash/gitstatus.py
@@ -4,7 +4,7 @@
 from __future__ import print_function
 
 # change those symbols to whatever you prefer
-symbols = {'ahead of': '↑·', 'behind': '↓·', 'prehash':':'}
+symbols = {'ahead of': 'A', 'behind': 'B', 'prehash':':'}
 
 from subprocess import Popen, PIPE
 
