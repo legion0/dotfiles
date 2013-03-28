e3ae3aa0748e04ac4b467657bc70f2d5b66437b0
Kibarski Jonathan <phenixdoc@gmail.com>
2013-03-29 00:09:28 +0200

    added shortpwd to ps1; moved ps1 to 1000 for more space

--- .bash/source/rc/1000.ps1.sh
+++ .bash/source/rc/1000.ps1.sh
@@ -2,6 +2,8 @@ ENDCOLOR='\[\e[m\]'
 COLOR1="\[\e[32m\]"
 COLOR2="\[\e[33m\]"
 COLOR3="\[\e[34m\]"
+COLOR_RED="\[\e[31m\]"
+
 # default cygwin
 #export PS1="\\[\\e]0;\\w\\a\\]\\n\\[\\e[32m\\]\\u@\\h \\[\\e[33m\\]\\w\\[\\e[0m\\]\\n\\\$ "
 
@@ -10,5 +12,13 @@ COLOR3="\[\e[34m\]"
 #export PS1="($?)\!|$COLOR1\u@\h$ENDCOLOR $COLOR2\w$ENDCOLOR$COLOR3$(__git_ps1 " (%s)")$ENDCOLOR\n$ "
 
 # for git-prompt.sh
-export PROMPT_START="($?)\!|$COLOR1\u@\h$ENDCOLOR $COLOR2\w$ENDCOLOR"
-export PROMPT_END="\n$ "
+
+function __format_ret {
+	r=$?
+	if (($r != 0)); then
+		echo "$r "
+	fi
+}
+
+export PROMPT_START="$COLOR_RED\$(__format_ret)$ENDCOLOR$COLOR1\u@\h$ENDCOLOR $COLOR2\$(__shortPWD)$ENDCOLOR"
+export PROMPT_END="\n\! $ "
