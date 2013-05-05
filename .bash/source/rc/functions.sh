d31fcd5082e08abf442a6536d280f033ade72de5
legion0 <phenixdoc@gmail.com>
2013-05-05 18:14:41 +0300

    Update functions.sh

--- .bash/source/rc/functions.sh
+++ .bash/source/rc/functions.sh
@@ -1,1 +1,2 @@
 function mkdir2 { mkdir "$1" && cd "$1"; }
+function git-dist { echo -ne "A "; git rev-list --left-right $1...$2 | cut -c 1-1 | grep -c \>; echo -ne "B "; git rev-list --left-right $1...$2 | cut -c 1-1 | grep -c \<; }
