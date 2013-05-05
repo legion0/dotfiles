fa0ec00f703a052474f5b8e7d3e595e00815a6be
legion0 <phenixdoc@gmail.com>
2013-05-05 18:41:45 +0300

    Update functions.sh

--- .bash/source/rc/functions.sh
+++ .bash/source/rc/functions.sh
@@ -1,2 +1,12 @@
-function mkdir2 { mkdir "$1" && cd "$1"; }
-function git-dist { echo -ne "A "; git rev-list --left-right $1...$2 | cut -c 1-1 | grep -c \>; echo -ne "B "; git rev-list --left-right $1...$2 | cut -c 1-1 | grep -c \<; }
+function mkdir2 {
+	mkdir "$1" && cd "$1";
+}
+function git-dist {
+	local str=`git rev-list --left-right $1...$2 | cut -c 1-1`;
+	str="${str//[^\<\>]}"
+	local str2="${str//[^\<]}"
+	local total="${#str}"
+	local ahead="${#str2}"
+	local behind="$(($total - $ahead))"
+	echo "A $ahead B $behind";
+}
