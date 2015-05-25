a850716a23f9f3f420d13c21bc79abab0660066b
legion0 <phenixdoc@gmail.com>
2015-05-25 12:09:20 +0300

    Create bash.sh

---
+++ .bash/source/rc/bash.sh
@@ -0,0 +1,29 @@
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+shopt -s globstar
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=10000
+HISTFILESIZE=10000
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+if [[ *"history -a"* != "${PROMPT_COMMAND}" ]]; then
+	PROMPT_COMMAND="${PROMPT_COMMAND} history -a;"
+fi
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# disable Ctrl+s terminal suspend
+stty -ixon
