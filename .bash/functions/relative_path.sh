b2e06c87cb76fffe584514533989930246907ab6
Jonathan Kibarsky <phenixdoc@gmail.com>
2015-09-27 09:40:10 +0300

    relative path bash function

---
+++ .bash/functions/relative_path.sh
@@ -0,0 +1,12 @@
+# Return relative path from canonical absolute dir path $1 to canonical
+# absolute dir path $2 ($1 and/or $2 may end with one or no "/").
+# Does only need POSIX shell builtins (no external command)
+relative_path () {
+    local common path up
+    common=${1%/} path=${2%/}/
+    while test "${path#"$common"/}" = "$path"; do
+        common=${common%/*} up=../$up
+    done
+    path=$up${path#"$common"/}; path=${path%/}; printf %s "${path:-.}"
+}
+
