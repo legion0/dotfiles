a6a1046790cfc4e410c85cacc5d08c7cc4d08535
Jonathan Kibarsky <phenixdoc@gmail.com>
2021-10-30 21:53:22 -0700

    fix bashrc manager glob issue and fix install issues

--- bashrc_manager/bashrc.sh
+++ bashrc_manager/bashrc.sh
@@ -1,7 +1,15 @@
 # TODO: Consider using https://github.com/bpkg/bpkg or https://github.com/basherpm/basher
 
-[[ ! -d "${HOME}/.bashrc.d" ]] || {
-  for f in "${HOME}/.bashrc.d/"*.sh; do
-    source "$f"
-  done
-}
+shopt -q nullglob && { bashrc_manager__shopt_nullglob_prev_state="-s" } || { bashrc_manager__shopt_nullglob_prev_state="-u" }
+[[ "${bashrc_manager__shopt_nullglob_prev_state}" == "-s" ]] || shopt -s nullglob
+
+bashrc_manager__files=("${HOME}/.bashrc.d/"*.sh)
+
+[[ "${bashrc_manager__shopt_nullglob_prev_state}" == "-s" ]] || shopt -u nullglob
+unset bashrc_manager__shopt_nullglob_prev_state
+
+for f in "${bashrc_manager__files[@]}"; do
+  source "$f"
+done
+
+unset bashrc_manager__files
--- bashrc_manager/install.sh
+++ bashrc_manager/install.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
-readonly SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+function main() {
+    readonly SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-[[ -d "${HOME}/.bashrc.d" ]] || {
-    echo "Creating directory: ${HOME}/.bashrc.d"
-    mkdir -p "${HOME}/.bashrc.d"
+    [[ -d "${HOME}/.bashrc.d" ]] || {
+        echo "Creating directory: ${HOME}/.bashrc.d"
+        mkdir -p "${HOME}/.bashrc.d"
+    }
+
+    # Backup the original bashrc file if its not a symlink, we will be sourcing it.
+
+    if [[ -e "${HOME}/.bashrc" ]] && [[ ! -L "${HOME}/.bashrc" ]]; then
+        echo "Fould old bashrc file at ${HOME}/.bashrc, attempting backup"
+        [[ ! -e "${HOME}/.bashrc.d/0.original_bashrc.sh" ]] || {
+            echo 1>&2 "ERROR: ${HOME}/.bashrc.d/0.original_bashrc.sh already exists, failed to backup current bashrc, aborting install"
+            exit 1
+        }
+        echo "Moving  ${HOME}/.bashrc to ${HOME}/.bashrc.d/0.original_bashrc.sh"
+        mv "${HOME}/.bashrc" "${HOME}/.bashrc.d/0.original_bashrc.sh"
+    fi
+
+    if [[ ! -e "${HOME}/.bashrc" ]] || [[ "$(readlink -f "${HOME}/.bashrc")" != "${SCRIPT_DIR}/bashrc.sh" ]]; then
+        echo "Removing old link ${HOME}/.bashrc -> $(readlink -f "${HOME}/.bashrc")"
+        rm -f "${HOME}/.bashrc"
+        echo "Linking ${HOME}/.bashrc -> ${SCRIPT_DIR}/bashrc.sh"
+        ln -s "${SCRIPT_DIR}/bashrc.sh" "${HOME}/.bashrc"
+    fi
 }
 
-# Backup the original bashrc file if its not a symlink, we will be sourcing it.
-[[ ! -f "${HOME}/.bashrc" ]] || [[ -f "${HOME}/.bashrc.d/original_bashrc.sh" ]] || {
-    echo "Moving  ${HOME}/.bashrc to ${HOME}/.bashrc.d/original_bashrc.sh"
-    mv "${HOME}/.bashrc" "${HOME}/.bashrc.d/original_bashrc.sh"
-}
-
-rm -f "${HOME}/.bashrc"
-
-# Install the new bashrc as a symlink.
-ln "${SCRIPT_DIR}/bashrc.sh" "${HOME}/.bashrc"
+main "$@"
